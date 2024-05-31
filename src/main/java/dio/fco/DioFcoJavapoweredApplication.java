04fd6cb15073607f6ac2d8a368b0399fc3da2273
Francisco Junior <jrpires.git@gmail.com>
2024-05-30 21:10:00 -0400

    correcao de bugs

--- src/main/java/dio/fco/DioFcoJavapoweredApplication.java
+++ src/main/java/dio/fco/DioFcoJavapoweredApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DioFcoJavapoweredApplication {
 
-	public String PORT= System.getenv("PORT");
-
 	public static void main(String[] args) {
 		SpringApplication.run(DioFcoJavapoweredApplication.class, args);
 	}
